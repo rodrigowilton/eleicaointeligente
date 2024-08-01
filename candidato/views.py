3bb98ec5d697c0d829b76cb43d4d5ed19b295545
rodrigo <rodrigowiltonalmeida@gmail.com>
2024-08-01 14:04:07 -0300

    princiap atualizada

--- candidato/views.py
+++ candidato/views.py
@@ -8,49 +8,74 @@ from lideranca.models import Contato, Lideranca
 from django.db.models import Count
 
 def generate_contact_graph():
-    # Get contact data
     contatos = Contato.objects.all()
     total_contatos = contatos.count()
 
-    # If there are no contatos, return an empty image
     if total_contatos == 0:
         return "", total_contatos
 
-    # Prepare data for the pie chart
-    # Create a count for each Lideranca
     lideranca_counts = contatos.values('lideranca__nome').annotate(count=Count('lideranca'))
     labels = [l['lideranca__nome'] for l in lideranca_counts]
     sizes = [l['count'] for l in lideranca_counts]
-    colors = plt.cm.Paired(range(len(labels)))
+    custom_colors = ['#FFA500', '#0d6efd']  # Verde e Laranja
 
-    # Generate a pie chart
-    fig, ax = plt.subplots(figsize=(2, 2))  # Adjusted size to 30%
-    wedges, texts, autotexts = ax.pie(sizes, colors=colors, autopct='%1.1f%%', startangle=140)
+    fig, ax = plt.subplots(figsize=(4, 4))
+    wedges, texts, autotexts = ax.pie(sizes, colors=custom_colors[:len(labels)], autopct='%1.1f%%', startangle=140)
 
-    # Improve the readability of labels and percentages
     for text in texts:
-        text.set_fontsize(8)  # Adjust font size for labels
+        text.set_fontsize(8)
     for autotext in autotexts:
-        autotext.set_fontsize(8)  # Adjust font size for percentages
+        autotext.set_fontsize(8)
 
-    ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    ax.axis('equal')
 
-    # Add a legend
     legend_labels = [f"{label} ({size})" for label, size in zip(labels, sizes)]
     ax.legend(wedges, legend_labels, title="Liderança", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
 
-    # Save the figure to a bytes buffer
     buf = io.BytesIO()
-    plt.savefig(buf, format='png', bbox_inches='tight')  # Adjust bbox_inches to fit the legend
+    plt.savefig(buf, format='png', bbox_inches='tight')
     buf.seek(0)
     image_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
     buf.close()
 
     return image_base64, total_contatos
 
+def generate_city_graph():
+    contatos = Contato.objects.all()
+    total_contatos = contatos.count()
+
+    if total_contatos == 0:
+        return "", total_contatos
+
+    cidades_contagem = contatos.values('cidade').annotate(contagem=Count('cidade'))
+    cidades = [cidade['cidade'] for cidade in cidades_contagem]
+    contagens = [cidade['contagem'] for cidade in cidades_contagem]
+    custom_colors = ['#0d6efd', '#FFA500']  # Verde e Laranja
+
+    fig, ax = plt.subplots(figsize=(2, 2))
+    wedges, texts, autotexts = ax.pie(contagens, colors=custom_colors[:len(cidades)], autopct='%1.1f%%', startangle=140)
+
+    for text in texts:
+        text.set_fontsize(8)
+    for autotext in autotexts:
+        autotext.set_fontsize(8)
+
+    ax.axis('equal')
+
+    legend_labels = [f"{cidade} ({contagem})" for cidade, contagem in zip(cidades, contagens)]
+    ax.legend(wedges, legend_labels, title="Cidade", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+
+    buf = io.BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight')
+    buf.seek(0)
+    image_base64 = base64.b64encode(buf.getvalue()).decode('utf-8')
+    buf.close()
+
+    return image_base64, total_contatos
 
 def index(request):
     contact_graph, total_contacts = generate_contact_graph()
+    city_graph, _ = generate_city_graph()  # Inclua o total_contatos se necessário
 
     # Obtenha todos os contatos
     contatos = Contato.objects.all()
@@ -65,16 +90,31 @@ def index(request):
     bairros_percentuais = [
         {
             'nome': bairro['bairro'],
-
+            'contagem': bairro['contagem'],
             'percentual': (bairro['contagem'] / total_contatos) * 100
         }
         for bairro in bairros_contagem
     ]
 
+    # Calcule a contagem de contatos por cidade
+    cidades_contagem = contatos.values('cidade').annotate(contagem=Count('cidade'))
+
+    # Calcule a porcentagem de contatos por cidade
+    cidades_percentuais = [
+        {
+            'nome': cidade['cidade'],
+            'contagem': cidade['contagem'],
+            'percentual': (cidade['contagem'] / total_contatos) * 100
+        }
+        for cidade in cidades_contagem
+    ]
+
     return render(request, 'principal.html', {
         'contact_graph': contact_graph,
+        'city_graph': city_graph,
         'total_contacts': total_contacts,
-        'bairros': bairros_percentuais,
+        'bairros': sorted(bairros_percentuais, key=lambda x: x['percentual'], reverse=True),
+        'cidades': sorted(cidades_percentuais, key=lambda x: x['percentual'], reverse=True),
     })
 
 def meta_create(request):
